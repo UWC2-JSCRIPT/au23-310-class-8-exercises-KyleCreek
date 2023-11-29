53f091252369fe892b42e20f7ba21d6eb481928c
KyleCreek <kyle.a.creek@gmail.com>
2023-11-28 20:08:00 -0800

    Updated Promise.js

--- promise.js
+++ promise.js
@@ -1,3 +1,4 @@
+/* 
 let myPromise = new Promise(function(resolve, reject) {
   setTimeout(function() {
     resolve();
@@ -11,4 +12,21 @@ myPromise
   .then(function(number) {
     console.log(number);
   });
+*/
 
+const randomNumberPromise = new Promise(function(resolve, reject){
+  // AFter One Second, Call Math Random
+  setTimeout(()=>{
+    const randomVal = Math.random();
+    // If random Value is less than .5, Resolve, else Reject
+    if (randomVal > 0.5){
+      resolve(randomVal);
+    } else {
+      reject(randomVal);
+    }
+  }, 1000)
+});
+
+randomNumberPromise.then((value)=>{
+  console.log('Success:', value);
+}).catch((value)=> {console.log('Failure', value)});
