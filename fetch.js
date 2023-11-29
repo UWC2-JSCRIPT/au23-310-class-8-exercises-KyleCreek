17acd23b75a8da681c386c1ef642c94341bd3a75
Kyle Creek <42050414+KyleCreek@users.noreply.github.com>
2023-11-29 15:07:16 -0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,23 +1,33 @@
-// create api-key.js file with const API_KEY="your_api_key" in this same directory to use
-const BASE_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const formEl = document.getElementById('best-books-form');
+const yearEl = document.getElementById('year');
+const monthEl = document.getElementById('month');
+const dateEl = document.getElementById('date');
 
-const url = `${BASE_URL}?q=tech&api-key=${API_KEY}`;
 
-fetch(url)
-  .then(function(data) {
-    return data.json();
-  })
-  .then(function(responseJson) {
-    console.log(responseJson);
+// Fetch Homework 
+const BASE_URL = 'https://api.nytimes.com/svc/books/v3/lists'
+const API_KEY = 'F0DYYfX3EAneOnBCXdgsItL6XYQmHAKD';
+const url = `${BASE_URL}/${date}/hardcover-fiction.json?api-key=${API_KEY}`;
 
-    let article = responseJson.response.docs[0];
-    console.log(article);
+const bookContainer = document.getElementById('books-container');
 
-    const mainHeadline = article.headline.main;
-    document.getElementById('article-title').innerText = mainHeadline;
 
-    if (article.multimedia.length > 0) {
-      const imgUrl = `https://www.nytimes.com/${article.multimedia[0].url}`;
-      document.getElementById('article-img').src = imgUrl;
-    }
-  });
+
+formEl.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const year = yearEl.value;
+  const month = monthEl.value;
+  const date = dateEl.value;
+  
+  const stringDate = `${year}-${month}-${date}`
+  const url = `${BASE_URL}/${stringDate}/hardcover-fiction.json?api-key=${API_KEY}`;
+  console.log(url);
+
+  // Fetch bestselling books for date and add top 5 to page
+	let test=fetch(url)
+		.then(function(data){
+			return(data.json());
+	});
+
+	console.log(test);
